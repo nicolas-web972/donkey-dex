f51a54047630571a9fc4cb3e2b634ce8a018fd37
Nicolas Chalono <nicolas.chalono@gmail.com>
2022-02-08 11:01:23 +0100

    component pokedex added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PokemonItemComponent } from './pokemon-item/pokemon-item.component';
+import { PokedexComponent } from './pages/pokedex/pokedex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PokemonItemComponent,
+    PokedexComponent,
   ],
   imports: [
     BrowserModule,
