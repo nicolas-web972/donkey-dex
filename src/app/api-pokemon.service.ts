7d022d1b3a235712c351ba48dfe879b8e9fe228f
Nathalie Lavroff <nathalielavroff@gmail.com>
2022-02-08 14:00:00 +0100

    update service

--- src/app/api-pokemon.service.ts
+++ src/app/api-pokemon.service.ts
@@ -6,10 +6,4 @@ import { Injectable } from '@angular/core';
 export class ApiPokemonService {
 
   constructor() { }
-
-  fetchKantoPokemon(){
-     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-     .then(response => response.json())
-     .then(allpokemon => console.log(allpokemon))
-    }
 }
