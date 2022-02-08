6106da7d192bf3dcda98470bd059152c0fa54243
Nicolas Chalono <nicolas.chalono@gmail.com>
2022-02-08 14:27:43 +0100

    api connected

--- src/app/api-pokemon.service.ts
+++ src/app/api-pokemon.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class ApiPokemonService {
 
   constructor() { }
+
+  fetchKantoPokemon(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+     .then(response => response.json())
+     .then((allpokemon) => {
+     allpokemon.results.forEach((pokemon:any) =>{
+       this.fetchPokemonData(pokemon);
+     })
+    })
+   }
+
+   fetchPokemonData(pokemon:any){
+    let url = pokemon.url // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+      fetch(url)
+      .then(response => response.json())
+      .then(function(pokeData){
+      console.log(pokeData)
+      })
+    }
+
 }
--- src/app/pokemon-item/pokemon-item.component.ts
+++ src/app/pokemon-item/pokemon-item.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiPokemonService } from '../api-pokemon.service';
 
 @Component({
   selector: 'app-pokemon-item',
   templateUrl: './pokemon-item.component.html',
   styleUrls: ['./pokemon-item.component.scss']
 })
-export class PokemonItemComponent implements OnInit {
 
-  constructor() { }
+export class PokemonItemComponent {
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private apiPokemon: ApiPokemonService
+  ) {}
 
+  // ngOnInit(): void {
+  // }
 }
